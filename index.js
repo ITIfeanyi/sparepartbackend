75ec645eb30dbedcb48cb29e4461585650796699
ITIfeanyi <ifeanyithankgodigweh@gmail.com>
2021-06-11 20:47:09 -0700

    added  || 3000  to the port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 const app = express();
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 //DB config
 // require("./config/db");
 
